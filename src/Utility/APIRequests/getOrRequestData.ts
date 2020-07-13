dd80a25e3e541e015ee77cbf580dc7710993dde6
Alex Persinger <apfbacc@gmail.com>
2020-07-13 11:27:07 -0400

    Fixed debug

--- src/Utility/APIRequests/getOrRequestData.ts
+++ src/Utility/APIRequests/getOrRequestData.ts
@@ -1,6 +1,7 @@
 const ax = require('axios').default;
 export function postServerData(body: any, endpoint: string, put: boolean): Promise<any> {
-    console.log(this.post_options);
+    console.log(`${process.env.REACT_APP_BASE_API_URL}${endpoint}`);
+    console.log(body);
     return ax.post(`${process.env.REACT_APP_BASE_API_URL}${endpoint}`, {
         body
     });
